19499ab525aee14c27ad4cf6862a0a98c094d31a
Twdor <adrianghitescu0713@gmail.com>
2021-09-20 13:21:04 +0300

    created initializer script file -> sql/init_db.sql

--- src/main/java/com/codecool/shop/controllers/ProductController.java
+++ src/main/java/com/codecool/shop/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductSubcategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductSubcategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
@@ -15,28 +17,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+    ProductDao productDataStore = ProductDaoMem.getInstance();
+    ProductSubcategoryDao productSubcategoryDataStore = ProductSubcategoryDaoMem.getInstance();
+    ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+    ProductService productService = new ProductService(productDataStore,productSubcategoryDataStore,productCategoryDataStore);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
-
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("categories", productService.getAllCategories());
+
         context.setVariable("category", productService.getProductCategory(1));
-        context.setVariable("products", productService.getProductsForCategory(1));
+//        context.setVariable("products", productService.getProductsForCategory(1));
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
         // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         // context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
+//        engine.process("product/just.html", context, resp.getWriter());
     }
 
 }
---
+++ src/main/sql/init_db.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS public.suppliers;
+CREATE TABLE public.suppliers (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    description text NOT NULL
+);
+
+DROP TABLE IF EXISTS public.product_categories;
+CREATE TABLE public.product_categories (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    department text NOT NULL
+);
+
+DROP TABLE IF EXISTS public.product_subcategories;
+CREATE TABLE public.product_subcategories (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    product_category_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.products;
+CREATE TABLE public.products (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    price decimal NOT NULL,
+    currency text NOT NULL,
+    description text NOT NULL,
+    product_subcategory_id integer NOT NULL,
+    product_supplier_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.customers;
+CREATE TABLE public.customers (
+    id serial NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    phone_number text,
+    country text,
+    city text,
+    zipcode text,
+    address text
+);
+
+DROP TABLE IF EXISTS public.orders;
+CREATE TABLE public.orders (
+    id serial NOT NULL PRIMARY KEY,
+    date date NOT NULL,
+    status text NOT NULL,
+    customer_id integer NOT NULL,
+    product_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.shopping_cards;
+CREATE TABLE public.shopping_cards (
+    id serial NOT NULL PRIMARY KEY,
+    customer_id integer NOT NULL,
+    product_id integer NOT NULL,
+    quantity integer NOT NULL
+);
