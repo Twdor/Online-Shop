0d4f096e62d98bd6e628f6f234992224f7399cc3
Twdor <adrianghitescu0713@gmail.com>
2021-09-30 23:06:37 +0300

    added more Dao methods

--- src/main/java/com/codecool/shop/service/Service.java
+++ src/main/java/com/codecool/shop/service/Service.java
@@ -3,9 +3,11 @@ package com.codecool.shop.service;
 import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.jdbc.*;
 import com.codecool.shop.dao.implementation.mem.*;
-import com.codecool.shop.model.*;
+import com.codecool.shop.models.*;
+import com.google.gson.JsonObject;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
@@ -15,6 +17,7 @@ public class Service {
     private ProductSubcategoryDao productSubcategoryDao;
     private UserDao userDao;
     private ShoppingCartDao shoppingCartDao;
+    private OrderDao orderDao;
 
     //return if db initializer already occurred
     private boolean isInitializer = false;
@@ -27,6 +30,7 @@ public class Service {
         this.productDao = ProductDaoMem.getInstance();
         this.userDao = UserDaoMem.getInstance();
         this.shoppingCartDao = ShoppingCartDaoMem.getInstance();
+        this.orderDao = OrderDaoMem.getInstance();
     }
 
     public Service(boolean Jdbc) throws SQLException {
@@ -36,6 +40,7 @@ public class Service {
         this.productDao = ProductDaoJdbc.getInstance();
         this.userDao = UserDaoJdbc.getInstance();
         this.shoppingCartDao = ShoppingCartDaoJdbc.getInstance();
+        this.orderDao = OrderDaoJdbc.getInstance();
         this.isInitializer = getProductCategoryById(1) != null;
     }
 
@@ -54,6 +59,10 @@ public class Service {
 
     public void add(ShoppingCartModel shoppingCartModel) { this.shoppingCartDao.add(shoppingCartModel); }
 
+    public void add(OrderModel orderModel) { this.orderDao.add(orderModel); }
+
+    public List<OrderModel> getAllOrdersBy(String userId) { return this.orderDao.getAll(userId); }
+
     public UserModel getUser(String email) { return userDao.find(email); }
 
     public UserModel getUser(Integer userId) { return userDao.find(userId); }
@@ -62,11 +71,32 @@ public class Service {
 
     public ShoppingCartModel getShoppingCartBy(String customerId, int productId) { return shoppingCartDao.find(customerId, productId); }
 
-    public List<ShoppingCartModel> getCustomerShoppingCart(String customerId) { return shoppingCartDao.getAll(customerId); }
+    public int getShoppingCartSize(String customerId) { return shoppingCartDao.getAll(customerId).size(); }
+
+    public List<JsonObject> getCustomerShoppingCart(String customerId) {
+        List<JsonObject> objects = new ArrayList<>();
+        for (ShoppingCartModel cart : shoppingCartDao.getAll(customerId)) {
+            JsonObject object = new JsonObject();
+            ProductModel product = productDao.find(cart.getProductId());
+
+            object.addProperty("cartId", cart.getId());
+            object.addProperty("productName", product.getName());
+            object.addProperty("productPrice", product.getPrice());
+            object.addProperty("productDefaultPrice", product.getDefaultPrice());
+            object.addProperty("productDefaultCurrency", String.valueOf(product.getDefaultCurrency()));
+            object.addProperty("quantity", cart.getQuantity());
+            object.addProperty("productId", cart.getProductId());
+
+            objects.add(object);
+        }
+        return objects;
+    }
+
+    public void update(UserModel user) { userDao.update(user); }
 
     public void deleteShoppingCart(int id) { shoppingCartDao.remove(id); }
 
-    public void updateShoppingCart(ShoppingCartModel shoppingCartModel) { shoppingCartDao.update(shoppingCartModel); }
+    public void update(ShoppingCartModel shoppingCartModel) { shoppingCartDao.update(shoppingCartModel); }
 
     public ProductCategoryModel getProductCategoryById(int id) {
         return productCategoryDao.find(id);
